37833ee239313983a51cc1520af20c413660e835
ThunderInTheGrass <propeller8924@gmail.com>
2021-07-23 23:13:09 +0800

    continue header

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,34 @@
 console.log('привет');
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  let mainSubnavItem = document.querySelectorAll('.main-subnav__item');
+  // let mainSubnavList = document.querySelectorAll('.main-subnav__list');
+  // let mainNavDropdown = document.querySelector('.main-nav__dropdown');
+
+  function mainSubnavHover() { 
+
+
+    mainSubnavItem.forEach((elem, index) => {
+      let parentList = elem.closest('.main-subnav__list');
+      let mainNavDropdown = elem.closest('.main-nav__dropdown');
+
+      elem.addEventListener('mouseenter', () => {
+        if (elem.children[1]) {
+          let navHeight = elem.children[1].offsetHeight;
+          if (parentList.offsetHeight < navHeight) {
+            parentList.style.height = `${navHeight}px`;
+          }
+          else{
+            elem.children[1].style.height = `${mainNavDropdown.offsetHeight}px`;
+          }
+        }
+      })
+      elem.addEventListener('mouseleave', () => {
+        parentList.style.height = 'auto';
+      })
+    })
+  }
+
+  mainSubnavHover();
+}
